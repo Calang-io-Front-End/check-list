661d3143f629eb7e19f1b2395d408bc8ea56365b
guilhermehub12 <guilhermee1312@hotmail.com>
2023-05-10 01:17:08 -0300

    :sparkles: feat: index page

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import Button from "components/Button";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex flex-col place-items-center">
+        <Image
+          className="relative rounded-full"
+          src="/assets/images/calang-io.jpg"
+          alt="Calangio Logo"
+          width={300}
+          height={250}
+          priority
+        />
+        <h1 className="left-0 top-0 flex w-full justify-center text-5xl font-bold">
+          Calang.io CheckList
+        </h1>
+        <Button name={"World"} className={"text-xl font-semibold"} />
+      </div>
+    </main>
+  );
+}
